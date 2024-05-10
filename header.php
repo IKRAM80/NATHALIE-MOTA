e7079b74d6f0c4a64ec4dd792520b1b4a8a75d20
Ikram Bchina <bchina.ikram@gmail.com>
2024-05-10 19:22:07 +0200

    git init header.php and footer.php

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nathalie Mota - Photographe Event</title>
+    <?php wp_head(); ?>
+</head>
+<body>
+    <?php wp_body_open(); ?>
+    <header class="header">
+
+
+
+    
+    </header>
