7cda54b1f32c483315c5d448f68b32ba21dc06e8
Ikram Bchina <bchina.ikram@gmail.com>
2024-05-13 20:05:47 +0200

    fichiers scss

--- functions.php
+++ functions.php
@@ -8,7 +8,3 @@ function register_my_menu(){
  }
  add_action('after_setup_theme', 'register_my_menu');
 
- add_action('wp_enqueue_scripts', 'nathalie_mota_enqueue_styles');
-function nathalie_mota_enqueue_styles() {
-    wp_enqueue_style('parent-style', get_template_directory_uri() . '/assets/scss/theme.css');
-}
