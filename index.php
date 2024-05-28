a16ca80148b0c1941800cfeb97bf932b00e2756a
Ikram Bchina <bchina.ikram@gmail.com>
2024-05-28 14:57:31 +0200

    le hero de la pade d'accueil et gallery phots

--- index.php
+++ index.php
@@ -8,10 +8,10 @@ get_header();
      <div class="hero__banner">
         <?php
             $args = array(
-                'post_type' => 'photo',
-                'posts_per_page' => 1,
-                'orderby' => 'rand',
-                'tax_query' => array(
+                'post_type' => 'photo',// Rechercher des articles du type photo
+                'posts_per_page' => 1,// Limiter la recherche à un seul article
+                'orderby' => 'rand',// Trier les résultats de manière aléatoire
+                'tax_query' => array(//Ajouter une requête de taxonomie pour filtrer les articles par le format paysage
                     array(
                         'taxonomy' => 'format',
                         'field' => 'slug',
@@ -20,10 +20,12 @@ get_header();
                 ),
             );
 
-            $query = new WP_Query($args);
-            if ($query->have_posts()) {
+            $query = new WP_Query($args);//Crée une nouvelle requête WordPress avec les arguments définis
+            if ($query->have_posts()) {//Vérifier si la requête a retourné des articles
+                //Boucle sur les articles trouvés, en définissant les données globales du poste actuel
                 while ($query->have_posts()) : $query->the_post();
                     ?>
+                    <!--Affiche l'image mise en avant de l'article avec l'URL de l'image et le titre de l'article comme attributs src et alt.-->
                     <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title_attribute(); ?>">
                     <?php
                 endwhile;
@@ -37,20 +39,23 @@ get_header();
 
      <div class="gallery-container">
             <?php
+            //Récupèrer le numéro de page de la requête actuelle. Si aucune page n'est définie, utilise 1 par défaut
                 $paged = get_query_var( 'paged', 1 );
                 $gallery = array(
-                    'post_type' => 'photo',
-                    'posts_per_page' => 8,
-                    'orderby' => 'date',
-                    'order' => 'ASC',
-                    'post_status' => 'publish',
-                    'paged' => $paged,
+                    'post_type' => 'photo',// Rechercher des articles du type photo
+                    'posts_per_page' => 8,// Limiter la recherche à huit articles par page
+                    'orderby' => 'date',// Trier les articles par date
+                    'order' => 'ASC',//Trier les articles par ordre croissant
+                    'post_status' => 'publish',//Rechercher uniquement les articles publiés
+                    'paged' => $paged,//Gérer la pagination en utilisant la variable $paged
                 );
-
+                //Créer une nouvelle requête WordPress avec les arguments définis
                 $query = new WP_Query($gallery);
 
-                if ($query->have_posts()){
+                if ($query->have_posts()){//Vérifier si la requête a retourné des articles
+                    //Boucle sur les articles trouvés en définissant les données globales du poste actuel
                     while ($query->have_posts()) : $query->the_post();
+                        //Inclut le modèle photo-block pour afficher chaque article
                         get_template_part('assets/template-parts/photo-block');
                     endwhile;
                 }
--- single-photo.php
+++ single-photo.php
@@ -3,78 +3,83 @@ get_header();
 ?>
     <main id="primary" class="site-main">
     <div class="page-content">
-         <div class="page-content">
-            <div class="main-content">
-                <div class="single-photo-content">
+         
+        <div class="main-content">
+            <div class="single-photo-content">
                     <div class="infos-container">
                         <p class=photo-title><?php echo get_the_title()?></p>
-                        <p> Référence : <span class="ref-value"><?php echo get_field('reference');?></span></p>
-                        <p> Catégorie : <?php echo get_the_terms(get_the_ID(), 'categorie')[0]->name; ?></p>
+                        <p> Référence : <span class="ref-value"><?php echo get_field('reference');?></span></p>                      
+                        <p> Catégorie : <?php echo get_the_terms(get_the_ID(), 'categorie')[0]->name; ?></p>                 
                         <p> Format : <?php echo get_the_terms(get_the_ID(), 'format') [0]->name; ?></p>
                         <p> Type : <?php echo get_field('type'); ?></p>
                         <p> Année : <?php echo get_field('date'); ?></p>
                         <div class="line">    
                             <hr>
+                        </div>
                     </div>
-                </div>
                 <div class="photos-container">
-            <?php if ( has_post_thumbnail() ) : ?>
-                <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="Photographie">
-            <?php else : ?>
-                <p>Aucune image mise en avant trouvée.</p>
-            <?php endif; ?>
-        </div>
+                    <?php if ( has_post_thumbnail() ) : ?>
+                      <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="Photographie">
+                    <?php else : ?>
+                    <p>Aucune image mise en avant trouvée.</p>
+                    <?php endif; ?>
+                </div>
             </div>
             <div class="contact-content">
                     <div class="contact">
                         <p class="contact-text">Cette photo vous intéresse ?</p>
                         <button class="btn contact-btn">Contact</button>
                     </div>
-                    <div class="preview">
+                    <div class="preview">                    
                         <?php
+                        // Récupère les objets des articles précédent et suivant
                         $previouspost = get_previous_post();
                         $nextpost = get_next_post();
                         ?>
                         <div class="arrows">
-                            <?php if ($previouspost) : ?>
-                                <?php $previous_photo = get_field('photo', $previouspost->ID); ?>
+                            <?php if ($previouspost) : ?>                                
+                                <?php
+                                // Récupère le champ personnalisé 'photo' pour l'article précédent (si nécessaire)
+                                     $previous_photo = get_field('photo', $previouspost->ID); ?>
                                 <a href="<?php echo get_permalink($previouspost); ?>" class="arrow-link arrow-left">
+                                    <!-- Affiche une flèche pour l'article précédent -->
                                     <img class="arrow arrow-left" src="<?php echo get_template_directory_uri(); ?>/assets/img/arrow-left.svg" alt="Arrow for previous picture">
                                     <div class="hover-thumbnail thumbnail-left">
-                                    <img src="<?php echo get_the_post_thumbnail_url($previouspost->ID, array(81, 71)) ?>" alt="Photo précédente">
+                                       <!-- Affiche la miniature de l'article précédent -->
+                                       <img src="<?php echo get_the_post_thumbnail_url($previouspost->ID, array(81, 71)) ?>" alt="Photo précédente">
+                                       <!--Affiche la miniature de la photo du post précédent avec une taille de 81x71 px-->
                                     </div>
                                 </a>
                             <?php endif; ?>
                             <?php if ($nextpost) : ?>
-                                <?php $next_photo = get_field('photo', $nextpost->ID); ?>
+                                <?php 
+                                // Récupère le champ personnalisé 'photo' pour l'article suivant (si nécessaire)
+                                    $next_photo = get_field('photo', $nextpost->ID); ?>
                                 <a href="<?php echo get_permalink($nextpost); ?>" class="arrow-link arrow-right">
+                                    <!-- Affiche une flèche pour l'article suivant -->
                                     <img class="arrow arrow-right" src="<?php echo get_template_directory_uri(); ?>/assets/img/arrow-right.svg" alt="Arrow for next picture">
                                     <div class="hover-thumbnail thumbnail-right">
+                                        <!-- Affiche la miniature de l'article suivant -->
                                         <img src="<?php echo get_the_post_thumbnail_url($nextpost->ID, array(81, 71)) ?>" alt="Photo suivante">
                                     </div>
                                 </a>
                             <?php endif; ?>
                         </div>
                     </div>
-                </div>
-    </div>
+            </div>
+        </div>
  
-    <div class="separator">
-    <hr>
-</div>
-<div>
-    <div class="gallery">
-        <p class="you-may-also-like">VOUS AIMEREZ AUSSI</p>
+    <div class="separator"><!--séparer le contenu visuellement par une ligne horizontale-->
+         <hr>
+    </div>
+    <div>
+       <div class="gallery">
+             <p class="you-may-also-like">VOUS AIMEREZ AUSSI</p>
         <div class="gallery-container">
             <?php
             $current_post_id = get_the_ID(); // Récupère l'ID de la photo actuelle 
-            $current_category = get_the_terms(get_the_ID(), 'category'); // Récupère les termes de taxonomie associés à cette photo
-
-            
-
+            $current_category = get_the_terms(get_the_ID(), 'category'); // Récupère les termes de taxonomie associés à cette photo        
             $category_name = $current_category ? $current_category[0]->slug : ''; // Obtient le slug de la première catégorie associée à cette photo
-
-            
             // Crée un tableau d'arguments pour interroger les publications
             $args = array(
                 'post_type' => 'photo', // Récupérer que les publications du type photo 
@@ -98,13 +103,13 @@ get_header();
             wp_reset_postdata(); // Réinitialise les données de publication après la boucle.
             ?>
         </div>
-    </div>
-</div>
-<div class="btn-container">
-    <a href="<?php echo home_url('/'); ?>">
+      </div>
+   </div>
+   <div class="btn-container">
+    <a href="<?php echo home_url('/'); ?>"><!--obtenir et imprimer l'URL de la page d'accueil du site WordPress-->
         <span class="btn home-btn">Toutes les photos</span>
     </a>
-</div>
+   </div>
 </div>
 
 </main><!-- #main -->
