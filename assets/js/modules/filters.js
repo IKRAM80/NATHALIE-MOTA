c1d63a32454f49d47412963e11adc2df6c21822f
Ikram Bchina <bchina.ikram@gmail.com>
2024-06-05 09:55:37 +0200

    filtres

--- assets/js/modules/filters.js
+++ assets/js/modules/filters.js
@@ -1,51 +1,56 @@
 // FILTERS
 
 function initializeFilters() {
-    let activeCategory = 'all';
-    let activeFormat = 'all';
-    let activeSortByDate = 'all';
+    let activeCategory = 'all';//Cette variable représente la catégorie active dans les filtres
+    let activeFormat = 'all';//représente le format actif dans les filtres.
+    let activeSortByDate = 'all';//représente le tri actif par date dans les filtres.
 
-    jQuery('#categories').val(activeCategory);
+    jQuery('#categories').val(activeCategory);// utiliser la bibliothèque jQuery pour sélectionner l'élément avec l'ID "categories" 
     jQuery('#formats').val(activeFormat);
     jQuery('#sort-by-date').val(activeSortByDate);
 
     function areFiltersActive() {
+    //vérifier si les filtres sont actifs en comparant les valeurs des variables "activeCategory",
+    // "activeFormat" et "activeSortByDate" avec la valeur "all
+    // Si au moins l'un des filtres est différent de "all", la fonction renvoie true, sinon elle renvoie false  
         return activeCategory !== 'all' || activeFormat !== 'all' || activeSortByDate !== 'all';
     }
 
-    // Event handler for filter changes
+    //gestionnaire d'événements "change" aux éléments avec les ID "categories", "formats" et "sort-by-date"
+    //Lorsque l'un de ces éléments change, la fonction anonyme sera appelée.
     jQuery('#categories, #formats, #sort-by-date').on('change', function() {
-        ajaxFilter();
+        ajaxFilter();//fonction appelée lorsque les filtres sont modifiés.
     });
 
     function ajaxFilter() {
+        //récupèrer les valeurs sélectionnées dans les éléments de filtre
         let category = jQuery('#categories').val();
         let format = jQuery('#formats').val();
         let sortByDate = jQuery('#sort-by-date').val();
         
-        // Update active filter states
+        //mettre à jour les variables avec ces valeurs puis effectuer une requête AJAX pour filtrer les données
         activeCategory = category;
         activeFormat = format;
         activeSortByDate = sortByDate;
 
-        //cacher le bouton charger plus si les filtres sont activés
+        //cacher le bouton charger plus si les filtres sont actifs
         if (areFiltersActive()) {
             jQuery('#load-more').hide();
         }
 
-        jQuery.ajax({
-            type: 'POST',
-            url: 'http://localhost:8080/nathalie-mota/wp-admin/admin-ajax.php', 
-            data: {
+        jQuery.ajax({//effectuer une requête AJAX en utilisant la bibliothèque jQuery.
+            type: 'POST',//type de requête HTTP à envoyer, dans ce cas, une requête POST
+            url: 'http://localhost:8080/nathalie-mota/wp-admin/admin-ajax.php',//la requête est envoyée à l'URL
+            data: {//données à envoyer avec la requête AJAX. Les données sont fournies sous la forme d'un objet JavaScript.
                 action: 'ajaxFilter',
                 category: category,
                 format: format,
                 sortByDate: sortByDate
             },
-            success: function(response) {
+            success: function(response) {//. La réponse de la requête AJAX est insérée dans cet élément
                 jQuery('.gallery-container').html(response);
 
-                // montrer le bouton charger plus si les filtres ne sont pas activés
+                // montrer le bouton charger plus si les filtres ne sont pas actifs
             }
         });
     }
--- assets/js/modules/loadMore.js
+++ assets/js/modules/loadMore.js
@@ -1,24 +1,24 @@
 // LOAD MORE
-const loadMore = jQuery("#load-more");
+const loadMore = jQuery("#load-more");//sélectionner l'élément avec l'ID "load-more"
 
-function initializeLoadMore() {
-    let currentPage = 1;
+function initializeLoadMore() {//fonction pour initialiser le comportement du bouton "Load More".
+    let currentPage = 1;//Cette variable sera utilisée pour suivre la page actuelle lors du chargement de plus de contenu
 
-    loadMore.on('click', function(event) {
-        event.preventDefault();
-        currentPage++;
+    loadMore.on('click', function(event) {//gestionnaire d'événements au clic sur le bouton
+        event.preventDefault();//empêcher le comportement par défaut du clic sur le bouton, ce qui évite que la page ne se recharge.
+        currentPage++;//incrémenter la valeur de la variable "currentPage" de 1 à chaque fois que le bouton "Load More" est cliqué
 
-        jQuery.ajax({
+        jQuery.ajax({//effectuer une requête AJAX en utilisant la bibliothèque jQuery avec ces détails
             type: 'POST',
             url: 'http://localhost:8080/nathalie-mota/wp-admin/admin-ajax.php', // Use the absolute URL provided by WordPress
-            dataType: 'json',
-            data: {
+            dataType: 'json',//on attend une réponse au format JSON.
+            data: {//les données envoyées sont l'action "loadMore"et la page actuelle "currentPage".
                 action: 'loadMore',
                 paged: currentPage,
             },
-            success: function(response) {
+            success: function(response) {//La réponse de la requête est ajoutée à la fin du contenu de cet élément.
                 jQuery('.gallery-container').append(response.html);
-
+                //déclarer une fonction appelée "checkIfMorePosts" prenant la réponse de la requête AJAX en tant que paramètre.
                 checkIfMorePosts(response);
             },
         });
@@ -26,11 +26,13 @@ function initializeLoadMore() {
 }
 
 function checkIfMorePosts(res) {
-    if (!res.has_more_posts) {
+    if (!res.has_more_posts) {//si la propriété "has_more_posts" de la réponse est fausse? il n'y a plus de publications à charger.
         loadMore.hide();
         console.log('Response : Has no more posts');
     } else {
         loadMore.show();
         console.log('Response : Has more posts');
     }
+//La fonction "checkIfMorePosts" est appelée pour vérifier si la réponse indique s'il y a plus de publications à charger ou non, 
+//et en fonction de cela, le bouton "Load More" est affiché ou masqué.
 }
